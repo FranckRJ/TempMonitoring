1419c47e0628e03bc1fbac3c5347aa23fbc47cc1
Franck W <franck.rj.w@gmail.com>
2021-01-18 22:24:35 +0100

    Meilleure conventions (un peu).

--- ArduinoMeasuring/conf.hpp
+++ ArduinoMeasuring/conf.hpp
@@ -1,27 +1,30 @@
 #ifndef CONF_HPP
 #define CONF_HPP
 
+#include <DHT.h>
+
 struct DefaultConf
 {
-  static inline const String WifiAccessName = "YOUR_ACCESS_NAME";
-  static inline const String WifiPassword = "YOUR_PASSWORD";
+  static inline const String wifiAccessName = "YOUR_ACCESS_NAME";
+  static inline const String wifiPassword = "YOUR_PASSWORD";
 
-  static inline const String ConnectionHost = "YOUR_HOST";
-  static inline const String ConnectionPort = "YOUR_PORT";
+  static inline const String connectionHost = "YOUR_HOST";
+  static inline const String connectionPort = "YOUR_PORT";
 
-  static inline const String WebServerRequestUrl = "YOUR_REQUEST_URL";
+  static inline const String webServerRequestUrl = "YOUR_REQUEST_URL";
 
-  static inline const int DhtPin = 4;
+  static inline const int dhtPin = 4;
+  static inline const int dhtType = DHT22;
 };
 
-# if __has_include("private.conf.hpp")
-#  include "private.conf.hpp"
+# if __has_include("_Conf.hpp")
+#  include "_Conf.hpp"
 # else
 
 struct Conf : public DefaultConf
 {
 };
 
-# endif // __has_include("private.conf.hpp")
+# endif // __has_include("_Conf.hpp")
 
 #endif // CONF_HPP
