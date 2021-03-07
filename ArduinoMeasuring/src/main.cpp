63bb4417eea01c48083d5444cb2fc209fbe7ea1e
Franck W <franck.rj.w@gmail.com>
2021-03-07 17:05:10 +0100

    Ajout d'un buffer de températures à envoyer plus tard.
    gdfgdfg
    
    
    sdfsfsdf

--- ArduinoMeasuring/src/main.cpp
+++ ArduinoMeasuring/src/main.cpp
@@ -1,15 +1,22 @@
 #include <Arduino.h>
 #include <DHT.h>
 #include <LowPower.h>
+#undef min // Pourquoi Arduino, POURQUOI ?????
+#undef max // :'(
+#include <etl/circular_buffer.h>
 
 #include "Conf.hpp"
 #include "EspAtCmdWrapper.hpp"
 #include "HttpRequestBuilder.hpp"
 
+constexpr int pause8SecDuration = 112;
+constexpr uint32_t pauseSecEstimateOfRealDuration = 976;
+
 namespace glob
 {
     DHT dht{Conf::dhtPin, Conf::dhtType};
     EspAtCmdWrapper espAtCmdWrapper{Conf::espPowerPin, LED_BUILTIN, Serial, Conf::wifiAccessName, Conf::wifiPassword};
+    etl::circular_buffer<float, 100> tempBuffer;
 } // namespace glob
 
 void deepSleepFor8s(int times = 1)
@@ -25,6 +32,41 @@ void deepSleepFor8s(int times = 1)
     CLKPR = oldClkPr;
 }
 
+float getTemp(int tries = 1)
+{
+    float temp = NAN;
+
+    do
+    {
+        temp = glob::dht.readTemperature(false, true);
+
+        if (!isnan(temp))
+        {
+            break;
+        }
+
+        --tries;
+    } while (tries > 0);
+
+    return temp;
+}
+
+bool sendTempRequestToEsp(float temp, int32_t delay = -1)
+{
+    String form = "value=";
+    form.concat(temp);
+
+    if (delay >= 0)
+    {
+        form.concat("&delay_in_sec=");
+        form.concat(delay);
+    }
+
+    const String request = HttpRequestBuilder::postForm(Conf::webServerRequestUrl, Conf::connectionHost, form);
+
+    return glob::espAtCmdWrapper.sendRequest(request, Conf::connectionHost, Conf::connectionPort);
+}
+
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
@@ -36,22 +78,48 @@ void setup()
 
 void loop()
 {
-    const float temp = glob::dht.readTemperature(false, true);
+    const float temp = getTemp(2);
+
     if (isnan(temp))
     {
+        glob::tempBuffer.push(NAN);
         digitalWrite(LED_BUILTIN, HIGH);
-        delay(100);
-        deepSleepFor8s();
-        return;
     }
+    else
+    {
+        if (sendTempRequestToEsp(temp))
+        {
+            while (!glob::tempBuffer.empty())
+            {
+                const float oldTemp = glob::tempBuffer.front();
 
-    const String form = "value=" + String(temp);
-    const String request = HttpRequestBuilder::postForm(Conf::webServerRequestUrl, Conf::connectionHost, form);
+                if (isnan(oldTemp))
+                {
+                    glob::tempBuffer.pop();
+                    continue;
+                }
 
-    glob::espAtCmdWrapper.sendRequest(request, Conf::connectionHost, Conf::connectionPort);
+                const auto delay = static_cast<int32_t>(pauseSecEstimateOfRealDuration * glob::tempBuffer.size());
+
+                if (sendTempRequestToEsp(oldTemp, delay))
+                {
+                    glob::tempBuffer.pop();
+                }
+                else
+                {
+                    glob::tempBuffer.push(NAN);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            glob::tempBuffer.push(temp);
+        }
+    }
 
     glob::espAtCmdWrapper.goToShutdownState();
     delay(100);
-    deepSleepFor8s(112);
+    deepSleepFor8s(pause8SecDuration);
     glob::espAtCmdWrapper.goToConnectedState();
 }
